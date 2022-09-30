af56fe9602deef9aeb86d6206d6f0eeb26f08ee5
OrererereO <516807140@qq.com>
2022-09-30 15:56:47 +0800

    完成链表相关题目

---
+++ src/main/java/com/obito/systemclass/class03/DeletesSpecifiedElement.java
@@ -0,0 +1,44 @@
+package com.obito.systemclass.class03;
+
+/**
+ * @author obito
+ */
+public class DeletesSpecifiedElement {
+
+    public static class Node {
+
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node deleteSpecifiedElement(Node head,int num) {
+
+        if (head == null) {
+            return null;
+        }
+
+        while (head != null) {
+            if (head.value != num) {
+                break;
+            }
+            head = head.next;
+        }
+
+        Node cur = head;
+        Node pre = head;
+        while (cur != null) {
+            if (cur.value == num) {
+                pre.next = cur.next;
+            }else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+
+        return head;
+    }
+}
---
+++ src/main/java/com/obito/systemclass/class03/Z-README.md
@@ -0,0 +1,6 @@
+1. ReverseSingleList 
+   反转单链表
+2. ReverseBothWayList 
+   反转双向链表
+3. DeletesSpecifiedElement 
+   删除单链表中指定的元素
