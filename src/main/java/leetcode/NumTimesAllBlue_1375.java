6d1939966759c9fbc27d19d18465248b3ecad229
bianjiahao <516807140@qq.com>
2023-06-14 15:46:44 +0800

    cody modify

---
+++ src/main/java/leetcode/NumTimesAllBlue_1375.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * @author admin
+ */
+public class NumTimesAllBlue_1375 {
+    
+    
+    public int numTimesAllBlue(int[] flips) {
+        if (flips == null || flips.length == 0) {
+            return 0;
+        }
+        int ans = 0;
+        
+        return ans;
+    }
+}
