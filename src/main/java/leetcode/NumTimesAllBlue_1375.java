27280b1c182a7d9ba3068f8ee872d06c83b66923
bianjiahao <516807140@qq.com>
2023-06-14 16:29:05 +0800

    cody modify

--- src/main/java/leetcode/NumTimesAllBlue_1375.java
+++ src/main/java/leetcode/NumTimesAllBlue_1375.java
@@ -5,13 +5,16 @@ package leetcode;
  */
 public class NumTimesAllBlue_1375 {
     
-    
+    // [3,2,4,1,5]
     public int numTimesAllBlue(int[] flips) {
-        if (flips == null || flips.length == 0) {
-            return 0;
+        int n = flips.length;
+        int ans = 0, right = 0;
+        for (int i = 0; i < n; i++) {
+            right = Math.max(right, flips[i]);
+            if (right == i + 1) {
+                ans++;
+            }
         }
-        int ans = 0;
-        
         return ans;
     }
 }
